a3a6bb60d7759475bdeb361d1b667060358b0cad
Alan Lemasney <alanlemasney34@yahoo.com>
2019-05-08 21:21:12 -0600

    Social-cards MVP complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return <div className="heading">
+    <h3>Lambda School</h3>
+    <p>@LambdaSchool - 26 Jan</p>
+    </div>
+}
+
+export default HeaderTitle;
